e9e554cdefab960f3f036cd5fedf626f0e7ac475
emanueledivizio <emadivizio@gmail.com>
2017-12-09 12:47:08 +0000

    removed explicit field resolvers

--- src/graphQL/Painting/index.js
+++ src/graphQL/Painting/index.js
@@ -1,17 +1,7 @@
 import schema from './painting.graphql';
 
 export const resolvers = {
-  Painting: {
-    painter(painting) {
-      return painting.author;
-    },
-    title(painting) {
-      return painting.title;
-    },
-    thumbnail(painting) {
-      return painting.image;
-    },
-  },
+  Painting: {},
 };
 
 export default schema;
